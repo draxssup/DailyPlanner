7393dfad7a1dc9f4998d996fb2cfa4c180041287
draxssup <dhruv.chatrani@gmail.com>
2024-03-04 17:07:29 +0530

    dailyplanner added

---
+++ python/src/timecli.py
@@ -0,0 +1,10 @@
+import sys
+import re
+from datetime import datetime
+
+class TimeTable:
+    __init__(self,da)
+def main():
+    date_format = '%Y-%m-%d'
+    if len(sys.argv) != 2:
+        sys.exit()
